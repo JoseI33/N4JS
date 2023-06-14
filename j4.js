e244eb5b6f40aec9d1dfaabc627ba927f0518296
Jose <jose.imhoff.20@gmail.com>
2023-06-14 00:11:31 -0300

    tp4

---
+++ j4.js
@@ -0,0 +1,18 @@
+/*1- Crea un objeto llamado auto que tenga algunas caracteristicas como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto. Crea los metodos para permitir encender y apagar el auto.
+
+function Auto (color, marca, modelo, encedido, apagado) {
+    this.color = color;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.encender = function(){
+        console.log(`${this.encedido} auto encendido. XD`);
+    }
+    this.apagar = function(){
+        console.log(`${this.apagado} El auto se apago. .:¬|`);
+    }
+}
+
+let auto = new Auto ('Amarillo', 'Fiat', 1995);
+let auto2 = new Auto ('Gris', 'Honda', 2010)
+console.log();*/
+
